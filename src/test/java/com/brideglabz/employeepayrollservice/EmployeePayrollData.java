b13faf9db41842e6f036f08e9929aada3bdccb9c
usha-12 <usha12ahirwar@gmail.com>
2022-11-28 13:07:09 +0530

    Create an employee payroll service to read and write employee payroll to a console

--- src/test/java/com/brideglabz/employeepayrollservice/EmployeePayrollData.java
+++ src/test/java/com/brideglabz/employeepayrollservice/EmployeePayrollData.java
@@ -1,2 +1,20 @@
-package com.brideglabz.employeepayrollservice;public class EmployeePayrollData {
+package com.brideglabz.employeepayrollservice;
+
+public class EmployeePayrollData {
+    public int employeeId;
+    public String employeeName;
+    public int employeeSalary;
+
+    public EmployeePayrollData(int employeeId, String employeeName, int employeeSalary) {
+        super();
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.employeeSalary = employeeSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayrollData [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeSalary="
+                + employeeSalary + "]";
+    }
 }
